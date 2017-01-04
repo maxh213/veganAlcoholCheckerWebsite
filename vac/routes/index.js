1de57327e39088ecb3d13d3cf41a460829d9b3c4
Max Harris <mh568@kent.ac.uk>
2017-01-04 02:51:18 +0000

    messed around with jade to generate tables and pass in information from js

--- vac/routes/index.js
+++ vac/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  	res.render('index', { title: 'Vegan Alcohol Checker' });
+});
+
+router.get('/alcohol', function(req, res, next) {
+	alcohols = [
+		{"name":'beer', "isVegan":'yes', "country":'UK'},
+	 	{"name":'wine', "isVegan":'yes', "country":'France'}
+	 ];
+  	res.render('layout', { alcohols: alcohols});
 });
 
 module.exports = router;
